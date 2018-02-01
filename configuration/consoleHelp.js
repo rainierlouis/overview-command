b6a8abcd7fd1138c57ab6e2de74daf41164f3da1
Selyuu <rainierlouis@gmail.com>
2018-02-01 12:16:34 +0100

    feat: implemented path method

--- configuration/consoleHelp.js
+++ configuration/consoleHelp.js
@@ -8,27 +8,32 @@ const log = console.log;
 const help = {
   menu: async () => {
     await asciimo.write("Overview", "isometric1", async art => {
-      await log(art.cyan);
-      await log(`
-
-	${chalk.dim("creates a visual file for your application structure - v0.3.0")}
-
-
-	${chalk.dim("Entry")}
-		${chalk.green(
-      "$ overview <entry>"
-    )} Declare entry point for the visualisation ${chalk.magenta.bold(
-        "required first"
-      )}
-
-	${chalk.dim("Reset")}
-		${chalk.green("<reset>")} ${chalk.cyan("[-r]")} Reset + delete visual folder
-
-	${chalk.dim("Help")}
-		${chalk.green("<help>")} ${chalk.cyan("[-h]")} Further detailing on options
-
-
-  	`);
+      //   await log(art.cyan);
+      //   await log(`
+      //
+      // ${chalk.dim('creates a visual file for your application structure - v0.3.0')}
+      //
+      //
+      // ${chalk.dim('Entry')}
+      // 	${chalk.green(
+      //   '$ overview <entry>'
+      //  )} Declare entry point for the visualisation ${chalk.magenta.bold(
+      //    'required first'
+      //   )}
+      //
+      // ${chalk.dim('Reset')}
+      // 	${chalk.green('<reset>')} ${chalk.cyan('[-r]')} Reset + delete visual folder
+      //
+      // ${chalk.dim('Path')}
+      // 	${chalk.green('<path>')} ${chalk.cyan(
+      //    '[-p]'
+      //   )} View the current path for the entry file
+      //
+      // ${chalk.dim('Help')}
+      // 	${chalk.green('<help>')} ${chalk.cyan('[-h]')} Further detailing on options
+      //
+      //
+      //  	`);
     });
   }
 };
