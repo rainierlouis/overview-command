d4467e36a03176f48903bcb4e8741b2d0ecf3ee7
Selyuu <rainierlouis@gmail.com>
2018-02-01 12:17:02 +0100

    refactor: file path method skimmed

--- configuration/parseModule.js
+++ configuration/parseModule.js
@@ -9,10 +9,9 @@ const parse = require("../parser/parse");
 const dataContent = {};
 
 const parsing = async entryPoint => {
-  const filePath = await path.join(__dirname, `../${entryPoint}`);
-  return await parse(filePath);
-
-  console.log("FILE PATH MY NINJA", filePath);
+  // const filePath = await path.join(__dirname, `../${entryPoint}`);
+  // await console.log('FILE PATH MY NINJA', filePath);
+  return await parse(entryPoint);
 };
 
 module.exports = { parsing };
