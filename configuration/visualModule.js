26de331e80dcc6c2005a20bb7417db847334a33e
Selyuu <rainierlouis@gmail.com>
2018-02-01 12:18:03 +0100

    refactor: file path method skimmed

--- configuration/visualModule.js
+++ configuration/visualModule.js
@@ -1,15 +1,18 @@
 const fs = require("fs");
 const mkdirp = require("mkdirp");
 const path = require("path");
+const opn = require("opn");
 
 // require Visual logic
 
 const filePath = path.join(__dirname, `../client/data/data.js`);
 
 const visualData = dataObj => {
-  fs.writeFile(filePath, dataObj, err => {
+  const fileContent = `${dataObj}`;
+  fs.writeFile(filePath, fileContent, err => {
     if (err) throw err;
   });
+  // continue visualising, return file
 };
 
 module.exports = { visualData };
