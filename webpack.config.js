18fee4d6833eb127ace422e62e314bc8441bcf75
Selyuu <rainierlouis@gmail.com>
2018-02-01 12:15:00 +0100

    fix: adjusted webpack logic

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: `./index.js`,
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "ovNpm.js"
+    filename: "bundle.js"
   },
   target: "node",
   resolve: { symlinks: false },
