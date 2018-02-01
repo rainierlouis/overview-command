7fea8423afbffe8ed4e2aef400db856f650926da
Selyuu <rainierlouis@gmail.com>
2018-02-01 12:15:55 +0100

    refactor: read file method

--- parser/parse.js
+++ parser/parse.js
@@ -1,22 +1,23 @@
-const esprima = require('esprima');
-const fs = require('fs');
-const babel = require('babel-core');
+const esprima = require("esprima");
+const fs = require("fs");
+const babel = require("babel-core");
 
-const readFile = filePath => new Promise((resolve, reject) => {
-  fs.readFile(__dirname + filePath, 'UTF8', (err, data) => {
-    if (err) reject(err);
-    else resolve(data);
+const readFile = filePath =>
+  new Promise((resolve, reject) => {
+    fs.readFile(__dirname + filePath, "UTF8", (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    });
   });
-});
 
 const parse = async (filePath, babelPlugins) => {
   const input = await readFile(filePath)
-    .then((fileContents) => {
+    .then(fileContents => {
       return babel.transform(fileContents, babelPlugins).code;
     })
     .catch(err => console.log(err));
 
-  return esprima.parseModule(input, {jsx: true});
+  return esprima.parseModule(input, { jsx: true });
 };
 
 module.exports = parse;
